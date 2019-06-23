250f87d9579b35342a82a9d32d802b2db01eafc6
Karolina Boboli <karolina@Karolinas-MacBook-Pro.local>
2019-06-23 17:03:36 +0200

    Added tests for fetching tweets

--- src/scripts/components/app.vue
+++ src/scripts/components/app.vue
@@ -17,31 +17,22 @@ export default {
             tweets: []
         }
     },
-    async mounted(){
-        console.log('app vue is ready');
-        const url = 'http://localhost:3000/tweets'
-
-        // make request - old way to do this:
-        // fetch(url)
-        //     .then((response) => {
-        //         return response.json();
-        //     })
-        //     .then((tweets) => {
-        //         this.tweets = tweets;
-        //     });
-
-        try {
-            const response = await fetch(url);
-            const tweets = await response.json();
-            this.tweets = tweets;
-        } catch (err) {
-            console.log(err);
+    methods: {
+        async fetchTweets() {
+            const url = 'http://localhost:3000/tweets'
+            try {
+                const response = await fetch(url);
+                return await response.json();
+            } catch (err) {
+                console.log(err);
+            }
         }
-
+    },
+    async mounted(){
+        this.tweets = await this.fetchTweets();
     }
 }
 </script>
 
 <style>
-
 </style>
--- test/specs/app.spec.js
+++ test/specs/app.spec.js
@@ -1,22 +1,12 @@
 // @ts-check
-import { assert } from 'chai'
-import { mount } from '@vue/test-utils'
+import { assert } from 'chai';
+import { mount } from '@vue/test-utils';
+import nock from 'nock';
 
 import App from "@/components/app";
 import TweetList from "@/components/tweet-list";
 
 suite('App', () => {
-    // Mocha nie uruchomi tego testu (it.skip)
-    // it.skip('should add 2 and 2', () => {
-    //     const input = 2 + 2;
-    //     const output =  4;
-    //     assert.equal(input, output);
-    // });
-    // it.skip('should add 4 and 4', () => {
-    //     const input = 4 + 4;
-    //     const output =  8;
-    //     assert.equal(input, output);
-    // });
 
     it('should render a single TweetList item', () => {
         const wrapper = mount(App, {
@@ -28,4 +18,49 @@ suite('App', () => {
         assert.ok(wrapper.contains(TweetList));
     })
 
+    it('function fetchTweets() exists', () => {
+        const wrapper = mount(App, {
+            stubs: {
+                // mockowanie całego tweeta
+                Tweet: true
+            }
+        });
+
+        // szukamy, gdzie jest fetchTweets()
+        console.log("wrapper", wrapper)
+        console.log('--------------------------------------')
+        console.log("wrapper vm", wrapper.vm);
+        console.log('--------------------------------------')
+        console.log("wrapper element", wrapper.element);
+        console.log('--------------------------------------')
+        console.log("wrapper options", wrapper.options);
+        console.log('--------------------------------------')
+
+        // sprawdzamy, czy fetchTweets() jest funkcją:
+        assert.isFunction(wrapper.vm.fetchTweets);
+    })
+
+    // nie działa z powodu braku modułu fs; nie wiadomo czemu
+    it.skip('should fetch tweets from external source via HTTP request', async () => {
+        const wrapper = mount(App, {
+            stubs: {
+                Tweet: true
+            }
+        });
+
+        nock('http://localhost:3000')
+            .get('/tweets')
+            .reply(200, [
+                {id: "1", body: "Tweet"}
+            ]);
+
+        const ft = wrapper.vm.fetchTweets;
+        const response = await ft();
+        console.log(response); // JSON
+        assert.lengthOf(response, 1);
+
+
+
+        
+    })
 })
